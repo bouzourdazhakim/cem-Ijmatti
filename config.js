aa4b9ade3d68b350163506f82577adc86451fea9
bouzourdazhakim <bouzourdazhakim@gmail.com>
2026-07-28 17:14:02 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ window.CEM_CONFIG = {
     cloudinaryPreset: "cem_preset",
 
     // 4. كلمة مرور لوحة التحكم الخاصة بالمؤسسة
-    adminPassword: "CEM19092002",
+    adminPassword: "admin123",
 
     // 5. روابط الاتصال والفضاء الرقمي للأولياء
     awliyaUrl: "https://awlyaa.education.dz/",
